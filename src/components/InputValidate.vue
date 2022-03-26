909c14104ace8cf36dc719cd6b30a6aa65b87aa0
ITIXYXYWA <ukv220@gmail.com>
2022-03-26 20:16:02 +0500

    fix regex

--- src/components/InputValidate.vue
+++ src/components/InputValidate.vue
@@ -14,7 +14,7 @@ export default {
   methods: {
     check(value: string): void {
       const re = new RegExp(
-        /^((([0-9A-Za-z]{1}[-0-9A-z\\.]{1,}[0-9A-Za-z]{1})|([0-9А-Яа-я]{1}[-0-9А-я\.]{1,}[0-9А-Яа-я]{1}))@([-A-Za-z]{1,}\.){1,2}[-A-Za-z]{2,})$/u
+        /^((([0-9A-Za-z]{1}[-0-9A-z\\.]{1,}[0-9A-Za-z]{1})|([0-9А-Яа-я]{1}[-0-9А-я\\.]{1,}[0-9А-Яа-я]{1}))@([-A-Za-z]{1,}\.){1,2}[-A-Za-z]{2,})$/u
       );
 
       this.statusValidate = value.match(re) ? "ok!" : "false";
